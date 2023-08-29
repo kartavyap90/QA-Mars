e0f5af3a15a2c04a61481ec396fe9aef6b31610b
kartavyap90 <kartavyap90@gmail.com>
2023-08-29 17:32:51 +1200

    Join button scenario completed

---
+++ Mars/Mars/Features/Mars.feature
@@ -0,0 +1,9 @@
+﻿Feature: This test suite contains test sceanrio for Mars project.
+
+
+
+Scenario: Check that New User can able to click on Join button.
+	Given Log into Mars portal
+	When New User can navigate and click on the Join button
+	Then The registration window should open
+
---
+++ Mars/Mars/Pages/Login_Or_Registration_Page.cs
@@ -0,0 +1,40 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mars.Pages
+{
+    public class Login_Or_Registration_Page
+    {
+        public void Navigate_To_URL(IWebDriver driver)
+        {
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("http://localhost:5000/");
+        }
+
+        public void Click_Join_Button(IWebDriver driver)
+        {
+            IWebElement join_button = driver.FindElement(By.XPath("//*[@id=\"home\"]/div/div/div[1]/div/button"));
+            join_button.Click();
+        }
+        
+        public void Registration_Window(IWebDriver driver)
+        {
+            IWebElement first_textbox = driver.FindElement(By.Name("firstName"));
+            if (first_textbox.Text == "" ) 
+            {
+                Console.WriteLine("Registration window was opened successfully, Test Passed.");
+            }
+
+        }
+
+        public void SignIn(IWebDriver driver)
+        {
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("http://localhost:5000/");
+        }
+    }
+}
